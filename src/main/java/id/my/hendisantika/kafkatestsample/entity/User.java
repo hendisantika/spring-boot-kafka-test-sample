ba618197a594fe63484554af53806591f086b9e6
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:03:33 +0700

    Add User entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/kafkatestsample/entity/User.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.kafkatestsample.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/24
+ * Time: 06:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity()
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+}
