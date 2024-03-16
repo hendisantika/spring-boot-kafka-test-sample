9dad128d6a66fd76758108c83c397a81e98333e4
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:36:33 +0700

    Add testGetByFirstName in UserRepositoryTest 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/kafkatestsample/repository/UserRepositoryTest.java
+++ src/test/java/id/my/hendisantika/kafkatestsample/repository/UserRepositoryTest.java
@@ -1,9 +1,17 @@
 package id.my.hendisantika.kafkatestsample.repository;
 
+import id.my.hendisantika.kafkatestsample.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,5 +33,23 @@ public class UserRepositoryTest {
     @Autowired
     private TestEntityManager testEntityManager;
 
+    @Test
+    void testGetByFirstName() {
+        testEntityManager.persist(stubUser("john", "Wick"));
+        testEntityManager.persist(stubUser("Robert", "McCall"));
+        testEntityManager.persist(stubUser("John", "Rambo"));
 
+        List<User> users = userRepository.getByFirstNameIgnoreCaseOrderByFirstNameAscLastNameAsc("John");
+
+        assertFalse(users.isEmpty());
+        assertEquals(2, users.size());
+        assertEquals("John", users.get(0).getFirstName());
+        assertEquals("Rambo", users.get(0).getLastName());
+        assertEquals("john", users.get(1).getFirstName());
+        assertEquals("Wick", users.get(1).getLastName());
+    }
+
+    private User stubUser(String firstName, String lastName) {
+        return new User(UUID.randomUUID().toString(), firstName, lastName);
+    }
 }
