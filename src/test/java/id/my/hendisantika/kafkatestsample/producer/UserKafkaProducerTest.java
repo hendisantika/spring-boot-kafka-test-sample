7ad124f4ec8666c0f5c4a83df6643dc0479e0247
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:30:53 +0700

    Add setUp in UserKafkaProducerTest  🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/kafkatestsample/producer/UserKafkaProducerTest.java
+++ src/test/java/id/my/hendisantika/kafkatestsample/producer/UserKafkaProducerTest.java
@@ -3,16 +3,22 @@ package id.my.hendisantika.kafkatestsample.producer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.kafkatestsample.kafka.producer.UserKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,5 +55,16 @@ class UserKafkaProducerTest {
         registry.add("spring.datasource.username", () -> "root");
         registry.add("spring.datasource.password", () -> "secret");
         registry.add("spring.flyway.enabled", () -> "false");
+    }
+
+    @BeforeAll
+    void setUp() {
+        DefaultKafkaConsumerFactory<String, String> consumerFactory = new DefaultKafkaConsumerFactory<>(getConsumerProperties());
+        ContainerProperties containerProperties = new ContainerProperties("com.madadipouya.kafka.user");
+        container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+        records = new LinkedBlockingQueue<>();
+        container.setupMessageListener((MessageListener<String, String>) records::add);
+        container.start();
+        ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic());
     }
 }
