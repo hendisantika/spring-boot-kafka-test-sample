82ed0e221d914fbfbc71e18a5dc7e92e906bf9f2
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:29:29 +0700

    Add testProduceAndConsumeKafkaMessage  🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/kafkatestsample/SpringBootKafkaTestSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/kafkatestsample/SpringBootKafkaTestSampleApplicationTests.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.kafkatestsample;
 
+import id.my.hendisantika.kafkatestsample.dto.UserDTO;
 import id.my.hendisantika.kafkatestsample.kafka.producer.UserKafkaProducer;
 import id.my.hendisantika.kafkatestsample.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,6 +14,11 @@ import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @Testcontainers
@@ -31,5 +39,19 @@ class SpringBootKafkaTestSampleApplicationTests {
         registry.add("spring.datasource.username", () -> "root");
         registry.add("spring.datasource.password", () -> "secret");
         registry.add("spring.flyway.enabled", () -> "false");
+    }
+
+    @Test
+    void testProduceAndConsumeKafkaMessage() {
+        ArgumentCaptor<UserDTO> captor = ArgumentCaptor.forClass(UserDTO.class);
+        UserDTO user = new UserDTO("11111", "John", "Wick");
+
+        userKafkaProducer.writeToKafka(user);
+
+        verify(userService, timeout(5000)).save(captor.capture());
+        assertNotNull(captor.getValue());
+        assertEquals("11111", captor.getValue().getUuid());
+        assertEquals("John", captor.getValue().getFirstName());
+        assertEquals("Wick", captor.getValue().getLastName());
     }
 }
