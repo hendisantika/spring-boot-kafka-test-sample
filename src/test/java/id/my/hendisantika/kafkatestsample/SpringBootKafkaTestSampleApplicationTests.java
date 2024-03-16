f507346fedb4f3f2093ddd77e5c703fa656b09f0
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:23:18 +0700

    Set Test Container 🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/kafkatestsample/SpringBootKafkaTestSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/kafkatestsample/SpringBootKafkaTestSampleApplicationTests.java
@@ -1,13 +1,35 @@
 package id.my.hendisantika.kafkatestsample;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.kafkatestsample.kafka.producer.UserKafkaProducer;
+import id.my.hendisantika.kafkatestsample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootKafkaTestSampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+    @Autowired
+    private UserKafkaProducer userKafkaProducer;
+    @MockBean
+    private UserService userService;
 
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:test");
+        registry.add("spring.datasource.driverClassName", () -> "org.h2.Driver");
+        registry.add("spring.datasource.username", () -> "root");
+        registry.add("spring.datasource.password", () -> "secret");
+        registry.add("spring.flyway.enabled", () -> "false");
+    }
 }
