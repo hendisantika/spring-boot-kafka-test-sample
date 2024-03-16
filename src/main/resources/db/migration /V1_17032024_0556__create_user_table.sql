a844dec530e95ae75e34f2fd63392040f98b22f3
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 05:57:29 +0700

    Add table script 🫸🌀✏️📗🐧🐳

---
+++ src/main/resources/db/migration /V1_17032024_0556__create_user_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE if NOT EXISTS users
+(
+    id
+    VARCHAR
+(
+    36
+) PRIMARY KEY,
+    first_name VARCHAR
+(
+    256
+) NOT NULL,
+    last_name VARCHAR
+(
+    256
+) NOT NULL,
+    CONSTRAINT uc_user_first_last_name UNIQUE
+(
+    first_name,
+    last_name
+),
+    INDEX idx_user_id
+(
+    id
+)
+    ) engine=InnoDB;
