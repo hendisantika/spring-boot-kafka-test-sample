5a343bb65154867a8a89516836e51da40282947f
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:37:11 +0700

    Add UserServiceTest 🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/kafkatestsample/service/UserServiceTest.java
@@ -0,0 +1,45 @@
+package id.my.hendisantika.kafkatestsample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-kafka-test-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/17/24
+ * Time: 06:36
+ * To change this template use File | Settings | File Templates.
+ */
+@Testcontainers
+@SpringBootTest
+public class UserServiceTest {
+
+    @Container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:8.0-debian"));
+    @Autowired
+    private UserService userService;
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("spring.datasource.url", () -> mySQLContainer.getJdbcUrl());
+        registry.add("spring.datasource.driverClassName", () -> mySQLContainer.getDriverClassName());
+        registry.add("spring.datasource.username", () -> mySQLContainer.getUsername());
+        registry.add("spring.datasource.password", () -> mySQLContainer.getPassword());
+        registry.add("spring.flyway.enabled", () -> "true");
+    }
+
+}
