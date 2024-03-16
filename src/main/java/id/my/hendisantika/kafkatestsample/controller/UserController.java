c5c538600c1a319e18f1bdcb9ba8ee2ace5cafed
hendisantika <hendisantika@yahoo.co.id>
2024-03-17 06:11:27 +0700

    Add generateRandomUser in UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/kafkatestsample/controller/UserController.java
+++ src/main/java/id/my/hendisantika/kafkatestsample/controller/UserController.java
@@ -1,12 +1,19 @@
 package id.my.hendisantika.kafkatestsample.controller;
 
 import com.github.javafaker.Faker;
+import id.my.hendisantika.kafkatestsample.dto.UserDTO;
 import id.my.hendisantika.kafkatestsample.kafka.producer.UserKafkaProducer;
 import id.my.hendisantika.kafkatestsample.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +36,11 @@ public class UserController {
     private final UserService userService;
 
     private final Faker faker;
+
+    @GetMapping("/random")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Create a user", description = "Creates a random user and write it to Kafka which is consumed by the listener")
+    public void generateRandomUser() {
+        kafkaProducer.writeToKafka(new UserDTO(UUID.randomUUID().toString(), faker.name().firstName(), faker.name().lastName()));
+    }
 }
